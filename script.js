2d5fef999035066bced1115a9ba118a272aeaeb2
yostina-safwat <eng_yostinasafwat@yahoo.com>
2025-05-25 11:35:18 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+// script.js
+const quotes = [
+    {
+      text: "It's not what happens to you, but how you react to it that matters.",
+      author: "Epictetus"
+    },
+    {
+      text: "Be yourself; everyone else is already taken.",
+      author: "Oscar Wilde"
+    },
+    {
+      text: "In the middle of every difficulty lies opportunity.",
+      author: "Albert Einstein"
+    },
+    {
+      text: "Success is not final, failure is not fatal: it is the courage to continue that counts.",
+      author: "Winston Churchill"
+    },
+    {
+      text: "Do what you can, with what you have, where you are.",
+      author: "Theodore Roosevelt"
+    }
+  ];
+  
+  let lastIndex = -1;
+  
+  document.getElementById("new-quote").addEventListener("click", () => {
+    let randomIndex;
+  
+   
+    do {
+      randomIndex = Math.floor(Math.random() * quotes.length);
+    } while (randomIndex === lastIndex);
+  
+    lastIndex = randomIndex;
+  
+    document.getElementById("quote-text").textContent = `"${quotes[randomIndex].text}"`;
+    document.getElementById("quote-author").textContent = `--${quotes[randomIndex].author}`;
+  });
+  
